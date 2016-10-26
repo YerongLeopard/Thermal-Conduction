5c8cc681ce060f37783d29c972ddc07264b92fdd
YerongLeopard <yerong.li@outlook.com>
2016-10-26 02:14:04 -0700

    script for calculating k_eff

--- data/analysis/kappa_eff/calculate_kappa_eff.py
+++ data/analysis/kappa_eff/calculate_kappa_eff.py
@@ -1,8 +1,47 @@
-import math
-import scipy
+import sys;
+sys.path.append("../include/")
+from analysis import *
+import numpy as np
+import scipy as sp
+import matplotlib
+try:
+   from PyQt4 import QtCore, QtGui
+except:
+   matplotlib.use("TkAgg")
+def K_eff(length):
+   isInteger = False; factor = 1.0
+   q1 = 2 * sp.pi / length 
+   rev_keff = 0
+   if (0 != length%2):
+      print("length has to be an even number") # Error message 
+      quit()
+   elif (0 != length%4):  # Ns/4 is a half integer
+      isInt = False
+   else:                  # Ns/4 is an integer
+      isInt = True
+   loop = np.arange(1,length,2) # Only do the summing up over odd nq's
+   for nq in loop:
+     q = q1 * nq
+     if isInt:
+        factor = sp.cos(q/2)
+     divider = float(length)/float(nq)
+     kappa = discrete_func([length],1,0.615140781881,divider)[0] # ratio
+     rev_keff += factor * sp.sin(q*length/4)/sp.sin(q/2)/kappa
+   rev_keff = 2*rev_keff/length
+   keff = 1/rev_keff
+   return keff
 def main():
-   
-   print("DEBUG")
-   return
+  L=list(np.arange(1000,2000, 1)); 
+  for l0 in L:
+      File = open('k_eff.dat', 'a');
+      keff=K_eff(l0)
+      print >> File, l0, keff
+      print l0,' ', keff
+      File.close() 
+  print("DEBUG")
+  return
+
+
 if __name__ == "__main__":
-    main()
+   DATA = np.loadtxt('6by6.dat') # Load data from the calculated results
+main()
