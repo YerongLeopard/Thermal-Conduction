d72ad5cb6d07c8ce961c49cf727448d442c620c6
YerongLeopard <yerong.li@outlook.com>
2016-10-26 16:17:13 -0700

    reorganize analysis include file

--- data/analysis/divergencefull/calculate-divergence.py
+++ data/analysis/divergencefull/calculate-divergence.py
@@ -15,7 +15,7 @@ import mpl_toolkits.mplot3d.axes3d as p3;
 L=list(np.arange(200,202, 1));
 for l0 in L:
   File = open('6by.dat', 'a');
-  y=discrete_func([l0],1,0.615140781881) # ratio
+  y=discrete_func([l0],1,0.615140781881)[0] # ratio
   print >> File,l0,y[0]
   print l0,' ', y[0]
   File.close()
--- data/analysis/include/analysis.py
+++ data/analysis/include/analysis.py
@@ -6,9 +6,9 @@ try:
 except:
    matplotlib.use("TkAgg")
 import pylab as pl;
-def discrete_func(List, kappa, MinPath, scale = 1):     # discrete_func1 takes wavevctor q as the major input
+def discrete_func(List, kappa, MinPath, scale = [1]):     # discrete_func1 takes wavevctor q as the major input
   Debyek=0.985 # Debye cut-off wavelength depend merely on the density
-  p=2; # p=2 has the real physics 
+  p=2          # p=2 has the real physics 
   N1=6
   N2=6
   ans=[]
@@ -22,7 +22,7 @@ def discrete_func(List, kappa, MinPath, scale = 1):     # discrete_func1 takes w
     last2=N2+1
     first3=-N3+1
     last3=N3+1
-    result=0.0
+    result=np.zeros(len(scale))
     for n1 in range(first1,last1):
       for n2 in range(first2,last2):
         for n3 in range(first3,last3):
@@ -30,18 +30,17 @@ def discrete_func(List, kappa, MinPath, scale = 1):     # discrete_func1 takes w
            and(+2*N2*N3*n1-2*N1*N3*n2+2*N1*N2*n3>-3*N1*N2*N3)and(+2*N2*N3*n1-2*N1*N3*n2+2*N1*N2*n3<3*N1*N2*N3+1)\
            and(+2*N2*N3*n1+2*N1*N3*n2-2*N1*N2*n3>-3*N1*N2*N3)and(+2*N2*N3*n1+2*N1*N3*n2-2*N1*N2*n3<3*N1*N2*N3+1)\
            and(+2*N2*N3*n1+2*N1*N3*n2+2*N1*N2*n3>-3*N1*N2*N3)and(+2*N2*N3*n1+2*N1*N3*n2+2*N1*N2*n3<3*N1*N2*N3+1)):
-             qc =q1*scale;  # calculating \kappa(qc=q1/4)
-             x1=n1/float(N1)
-             x2=n2/float(N2)
-             x3=n3/float(N3)             
-             sq=x1**2+x2**2+x3**2
-             if (0!=sq):
-               i+=1
-               v=(n1/float(N1)+n2/float(N2)-n3/float(N3))/np.sqrt(3.0)
-               k=np.sqrt(x1**2+x2**2+x3**2)
-#               if (v>k):  # DEBUG
-#                  print v,k # DEBUG
-               result=result+((v**2)/sq)*((Debyek/k)**p)/(1+4*((np.sin(qc/2))**2)*(MinPath*(v/k)*((Debyek/k)**p)+(MinPath*(v/k)*((Debyek/k)**p))**2))
+             for it, scl in enumerate(scale):
+               qc =q1*scl     # calculating \kappa(qc=q1*scl)
+               x1=n1/float(N1)
+               x2=n2/float(N2)
+               x3=n3/float(N3)             
+               sq=x1**2+x2**2+x3**2
+               if (0!=sq):
+                 i+=1
+                 v=(n1/float(N1)+n2/float(N2)-n3/float(N3))/np.sqrt(3.0)
+                 k=np.sqrt(x1**2+x2**2+x3**2)
+                 result[it]=result[it]+((v**2)/sq)*((Debyek/k)**p)/(1+4*((np.sin(qc/2))**2)*(MinPath*(v/k)*((Debyek/k)**p)+(MinPath*(v/k)*((Debyek/k)**p))**2))
     result=result/(N3*N1*N2*4)*(3-p)*((np.cos(qc/2))**2)
     ans.append(result*kappa)
     # print i,'==',92*N2*4*N3,'(N=',N3,')' # DEBUG
