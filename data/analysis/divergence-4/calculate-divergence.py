05596024e786531fb03e19182aeed4ed657b5277
YerongLeopard <yerong.li@outlook.com>
2016-10-26 16:16:15 -0700

    fix calcute kappa(q*4)

--- data/analysis/divergence-4/calculate-divergence.py
+++ data/analysis/divergence-4/calculate-divergence.py
@@ -10,10 +10,10 @@ except:
    matplotlib.use("TkAgg")
 import pylab as pl;
 import matplotlib.pyplot as plt;
-L=list(np.arange(400,12000*4, 4));
+L=list(np.arange(432,12000*4, 4));
 for l0 in L:
   File = open('6by.dat', 'a');
-  y=discrete_func([l0],1,0.615140781881,2) # ratio
+  y=discrete_func([l0],1,0.615140781881,[4])[0] # ratio
   print >> File,l0,y[0]
   print l0,' ', y[0]
   File.close()
