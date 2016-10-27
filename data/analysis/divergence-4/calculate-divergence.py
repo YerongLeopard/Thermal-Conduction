fix calculation of k_eff in which the sample length is multiple of 4

data/analysis/divergence-4/calculate-divergence.py
@@ -10,7 +10,7 @@ except:
    matplotlib.use("TkAgg")
 import pylab as pl;
 import matplotlib.pyplot as plt;
-L=list(np.arange(432,12000*4, 4));
+L=list(np.arange(9500,12000*4, 4));
 for l0 in L:
   File = open('6by.dat', 'a');
   y=discrete_func([l0],1,0.615140781881,[4])[0] # ratio

data/analysis/divergencefull/calculate-divergence.py
@@ -12,10 +12,10 @@ import pylab as pl;
 import matplotlib.pyplot as plt;
 import scipy.optimize as optimization;
 import mpl_toolkits.mplot3d.axes3d as p3;
-L=list(np.arange(200,202, 1));
+L=list(np.arange(20,21, 1));
 for l0 in L:
   File = open('6by.dat', 'a');
-  y=discrete_func([l0],1,0.615140781881)[0] # ratio
+  y=discrete_func([l0],1,0.615140781881,[1])[0] # ratio
   print >> File,l0,y[0]
   print l0,' ', y[0]
   File.close()