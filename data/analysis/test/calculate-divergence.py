2fe4204dd9533bc3993c0e5bf98517c3699e82d9
YerongLeopard <yerong.li@outlook.com>
2016-12-03 19:42:17 -0800

    test folder

---
+++ data/analysis/test/calculate-divergence.py
@@ -0,0 +1,23 @@
+import sys;
+sys.path.append("../include/")
+from analysis import *
+import scipy as sp;
+import numpy as np;
+import matplotlib;
+try:
+   from PyQt4 import QtCore, QtGui
+except:
+   matplotlib.use("TkAgg")
+import pylab as pl;
+import matplotlib.pyplot as plt;
+import scipy.optimize as optimization;
+import mpl_toolkits.mplot3d.axes3d as p3;
+L=list(np.arange(20,21, 1));
+for l0 in L:
+  File = open('6by.dat', 'a');
+  y=discrete_func([l0],1,0.615140781881,[1])[0] # ratio
+  print >> File,l0,y[0]
+  print l0,' ', y[0]
+  File.close()
+print '\nfinished.\n'
+
