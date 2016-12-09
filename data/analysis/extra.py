8ceb2cc5c721c588b7008cb4c21af8207e050e51
YerongLeopard <yerong.li@outlook.com>
2016-12-08 18:29:56 -0800

    formatting figures

--- data/analysis/extra.py
+++ data/analysis/extra.py
@@ -18,35 +18,32 @@ x1=[]
 x2=[]
 x3=[]
 for line in data:
-  a.append(line[[1]]*3)
-  b.append(line[[2]]*3)
-  c.append(line[[3]]*3)
+  a.append(line[[1]])
+  b.append(line[[2]])
+  c.append(line[[3]])
   x1.append(1.0/line[0]**2)
   x2.append(1.0/line[0])
   x3.append(line[0]**2)
 pl.figure(1)
 pl.plot(x1,a,'ob',markersize=8,label="$N_x>10$")
 pl.plot(x1,a,'-b',markersize=8)
-pl.xlabel('$1/N_y^2$',fontsize=25)
-h=pl.ylabel('$A$',fontsize=25)
+pl.xlabel('$1/N_y^2$',fontsize=22)
+h=pl.ylabel('$A$',fontsize=22)
 h.set_rotation(0)
-pl.legend(loc='best')
 
 pl.figure(2)
 pl.plot(x2,b,'ob',markersize=8,label="$N_x>10$")
 pl.plot(x2,b,'-b',markersize=8)
-pl.xlabel('$1/N_y$',fontsize=25)
-h=pl.ylabel('$B$',fontsize=25)
+pl.xlabel('$1/N_y$',fontsize=22)
+h=pl.ylabel('$B$',fontsize=22)
 h.set_rotation(0)
-pl.legend(loc='best')
 
 pl.figure(3)
 pl.plot(x3,c,'ob',markersize=8,label="$N_x>10$")
 pl.plot(x3,c,'-b',markersize=8)
-pl.xlabel('$N_y^2$',fontsize=25)
-h=pl.ylabel('$C$',fontsize=25)
+pl.xlabel('$N_y^2$',fontsize=22)
+h=pl.ylabel('$C$',fontsize=22)
 h.set_rotation(0)
-pl.legend(loc='best')
 #########################################50
 data=pl.loadtxt('ABC50.dat')
 a=[]
@@ -56,21 +53,18 @@ x1=[]
 x2=[]
 x3=[]
 for line in data:
-  a.append(line[[1]]*3)
-  b.append(line[[2]]*3)
-  c.append(line[[3]]*3)
+  a.append(line[[1]])
+  b.append(line[[2]])
+  c.append(line[[3]])
   x1.append(1.0/line[0]**2)
   x2.append(1.0/line[0])
   x3.append(line[0]**2)
 pl.figure(1)
 pl.plot(x1,a,'oy',markersize=8,label="$N_x>50$")
 pl.plot(x1,a,'-y',markersize=8)
-pl.legend(loc='best')
-
 ax = plt.gca()
 # Customize tick marks
-ax.tick_params(which='both',width=2,length=8) # setting length and width of tick marks
-
+ax.tick_params(which='both',width=2,length=12) # setting length and width of tick marks
 # Customize tick labels
 for idx, tick in enumerate(ax.xaxis.get_major_ticks()):
     tick.label1.set_fontsize(12)
@@ -80,16 +74,13 @@ for tick in ax.yaxis.get_major_ticks():
     tick.label1.set_fontsize(12)
     tick.label1.set_fontweight('bold')
 
-pl.legend(loc='best',fontsize=24)
 
 pl.figure(2)
 pl.plot(x2,b,'oy',markersize=8,label="$N_x>50$")
 pl.plot(x2,b,'-y',markersize=8)
-pl.legend(loc='best')
-
 ax = plt.gca()
 # Customize tick marks
-ax.tick_params(which='both',width=2,length=8) # setting length and width of tick marks
+ax.tick_params(which='both',width=2,length=12) # setting length and width of tick marks
 
 # Customize tick labels
 for idx, tick in enumerate(ax.xaxis.get_major_ticks()):
@@ -103,12 +94,9 @@ for tick in ax.yaxis.get_major_ticks():
 pl.figure(3)
 pl.plot(x3,c,'oy',markersize=8,label="$N_x>50$")
 pl.plot(x3,c,'-y',markersize=8)
-pl.legend(loc='best',fontsize=24)
-
 ax = plt.gca()
 # Customize tick marks
-ax.tick_params(which='both',width=2,length=8) # setting length and width of tick marks
-
+ax.tick_params(which='both',width=2,length=12) # setting length and width of tick marks
 # Customize tick labels
 for idx, tick in enumerate(ax.xaxis.get_major_ticks()):
     tick.label1.set_fontsize(12)
@@ -118,7 +106,6 @@ for tick in ax.yaxis.get_major_ticks():
     tick.label1.set_fontsize(12)
     tick.label1.set_fontweight('bold')
 
-pl.legend(loc='best',fontsize=24)
 ########################################400
 data=pl.loadtxt('ABC400.dat')
 a=[]
@@ -128,26 +115,31 @@ x1=[]
 x2=[]
 x3=[]
 for line in data:
-  a.append(line[[1]]*3)
-  b.append(line[[2]]*3)
-  c.append(line[[3]]*3)
+  a.append(line[[1]])
+  b.append(line[[2]])
+  c.append(line[[3]])
   x1.append(1.0/line[0]**2)
   x2.append(1.0/line[0])
   x3.append(line[0]**2)
 pl.figure(1)
 pl.plot(x1,a,'or',markersize=8,label="$N_x>400$")
 pl.plot(x1,a,'-r',markersize=8)
-pl.legend(loc='best')
+pl.xlim([0.0,0.26])
+pl.legend(loc='best',fontsize=20)
 
 pl.figure(2)
 pl.plot(x2,b,'or',markersize=8,label="$N_x>400$")
 pl.plot(x2,b,'-r',markersize=8)
-pl.legend(loc='best')
+pl.xlim([0.00,0.55])
+pl.ylim([0.4,1.02])
+pl.legend(loc='best',fontsize=20)
 
 pl.figure(3)
 pl.plot(x3,c,'or',markersize=8,label="$N_x>400$")
 pl.plot(x3,c,'-r',markersize=8)
-pl.legend(loc='best')
+pl.xlim([0,105])
+pl.ylim([-0.8,0.02])
+pl.legend(loc='best',fontsize=20)
 
 '''
 ########################################800
@@ -159,9 +151,9 @@ x1=[]
 x2=[]
 x3=[]
 for line in data:
-  a.append(line[[1,2,3]]*3)
-  b.append(line[[1,2,3]]*3)
-  c.append(line[[1,2,3]]*3)
+  a.append(line[[1,2,3]])
+  b.append(line[[1,2,3]])
+  c.append(line[[1,2,3]])
   x1.append(1.0/line[0]**2)
   x2.append(1.0/line[0])
   x3.append(line[0]**2)
@@ -185,9 +177,9 @@ x1=[]
 x2=[]
 x3=[]
 for line in data:
-  a.append(line[[1,2,3]]*3)
-  b.append(line[[1,2,3]]*3)
-  c.append(line[[1,2,3]]*3)
+  a.append(line[[1,2,3]])
+  b.append(line[[1,2,3]])
+  c.append(line[[1,2,3]])
   x1.append(1.0/line[0]**2)
   x2.append(1.0/line[0])
   x3.append(line[0]**2)
